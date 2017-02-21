4cbc39e7b55714c4571789a38e4e65c6edc70608
Ted Werbel <werbel.ted@gmail.com>
2017-02-20 22:10:34 -0500

    added axios post request to signinUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
+import axios from 'axios'
+
+const ROOT_URL = 'http://localhost:3090';
+
 export const signinUser = ({ email, password }) => {
-  // Submit email/password to the server
+  return (dispatch) => {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password })
 
-  // If request is good...
-  // - Update state to indicate user is authenticated
-  // - Save the JWT token
-  // - Redirect to the rout './feature'
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - Redirect to the rout './feature'
 
-  // If request is bad...
-  // - Show an error to the user
+    // If request is bad...
+    // - Show an error to the user
+  }
 }
