f3a72b5954995c5e606bcc1766a5d07a12b5c8a5
Ted Werbel <werbel.ted@gmail.com>
2017-02-20 21:38:19 -0500

    integrated redux form reducer and started the signin form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+const rootReducer = combineReducers({ form });
 
 export default rootReducer;
