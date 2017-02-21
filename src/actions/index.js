5c00025a4f90f612cebdf626ea5a20c6abc677d6
Ted Werbel <werbel.ted@gmail.com>
2017-02-21 18:57:06 -0500

    downgraded redux-form and removed unnecessary logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,15 @@ import axios from 'axios'
 
 const ROOT_URL = 'http://localhost:3090';
 
-export function signinUser({ email, password }) {
+export const signinUser = ({ email, password }) => {
   return (dispatch) => {
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        console.log("Login success!", response);
+      })
+      .catch(() => {
+        console.log("Error!");
+      });
   }
 }
