90ce5e7fa08e94b61b34bde593edc848b480be10
Ted Werbel <werbel.ted@gmail.com>
2017-02-20 22:01:02 -0500

    added notes on how the signinUser action should function

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const signinUser = ({ email, password }) => {
+  // Submit email/password to the server
+
+  // If request is good...
+  // - Update state to indicate user is authenticated
+  // - Save the JWT token
+  // - Redirect to the rout './feature'
+
+  // If request is bad...
+  // - Show an error to the user
+}
