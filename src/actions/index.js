04aef8fb9affbf0df1a91eb002c964325ac11abb
Ted Werbel <werbel.ted@gmail.com>
2017-02-20 22:46:49 -0500

    reverted back to es6 fat arrow functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const ROOT_URL = 'http://localhost:3090';
 
-export function signinUser({ email, password }) {
-  return function(dispatch) {
+export const signinUser = ({ email, password }) => {
+  return (dispatch) => {
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password });
 
