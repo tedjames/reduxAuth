e6fccc096b185d3a30e1370543e0d0839d1e0132
Ted Werbel <werbel.ted@gmail.com>
2017-02-20 21:24:37 -0500

    added placeholder components and integrated react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +11,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
