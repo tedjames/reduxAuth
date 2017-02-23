5dc1087939b5421d7ce2b299fac10eabc32a2515
Ted Werbel <werbel.ted@gmail.com>
2017-02-22 23:39:28 -0500

    modified actions and added routes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { browserHistory } from 'react-router'
-import { AUTH_USER, AUTH_ERROR } from './types'
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types'
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -18,6 +18,28 @@ export const signinUser = ({ email, password }) => {
       });
   }
 }
+
+export const signupUser = ({ email, password }) => {
+  return (dispatch) => {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER }); // Update authentication status
+        localStorage.setItem('token', response.data.token) // Save the JWT token
+        browserHistory.push("./feature"); // Redirect the user to the feature route
+      })
+      .catch(() => {
+        dispatch(authError('Error occured! Could not make account...'));
+      });
+  }
+}
+
+export const signoutUser = () => {
+  localStorage.removeItem('token'); // Delete JWT token
+  return {
+    type: UNAUTH_USER
+  };
+};
 
 export const authError = (error) => {
   return {
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reduxThunk from 'redux-thunk'
 import App from './components/app'
 import Signin from './components/auth/signin'
 import Signout from './components/auth/signout'
+import Signup from './components/auth/signup'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -18,6 +19,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <Route path="signin" component={Signin}></Route>
         <Route path="signout" component={Signout}></Route>
+        <Route path="signup" component={Signup}></Route>
       </Route>
     </Router>
   </Provider>
