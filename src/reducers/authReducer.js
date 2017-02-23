d43607994c3201fe254096d3a4a82b8f1da30a3f
Ted Werbel <werbel.ted@gmail.com>
2017-02-23 15:08:08 -0500

    added message to authReducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,7 +1,8 @@
 import {
   AUTH_USER,
   UNAUTH_USER,
-  AUTH_ERROR
+  AUTH_ERROR,
+  FETCH_MESSAGE
 } from '../actions/types'
 
 const INITIAL_STATE = {};
@@ -15,6 +16,8 @@ export default function(state = INITIAL_STATE, action) {
       return { ...state, authenticated: false };
     case AUTH_ERROR:
       return { ...state, error: action.payload };
+    case FETCH_MESSAGE:
+      return { ...state, message: action.payload }
     default:
       return state;
   }
